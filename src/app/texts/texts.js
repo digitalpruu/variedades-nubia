bbc08219d9d3d6847e847ca33d4feb8b583f2746
kevinpicon01 <digitalpruu@gmail.com>
2024-09-27 18:55:14 -0500

    Changed media, color, texts

--- src/app/texts/texts.js
+++ src/app/texts/texts.js
@@ -1,9 +1,10 @@
-const title = "Titulo del emprendimiento";
-const email = "Email del emprendimiento";
+const title = "Variedades Sofia";
+const email = "variednubia@gmail.com";
 const mensajeWhatsApp = encodeURIComponent(
-    `¡Hola! Estoy muy interesado en tus productos de ${title} y me gustaría recibir más información sobre ellos. Espero poder hablar pronto contigo para aclarar algunas dudas. ¡Gracias!`
+    "¡Hola! Estoy interesado en conocer más sobre los productos de " + title + " y me gustaría obtener más información. ¡Gracias!"
 );
-const numeroWhatsApp = "Numero del emprendimiento";
+const numeroWhatsApp = "+573503470248";
+
 const textos = {
     index: {
         title: title,
@@ -12,25 +13,25 @@ const textos = {
         title: title,
     },
     about: {
-        description1: "Descripción del emprendimiento parrafo uno",
-        description2: "Descripcion del emprendimiento parrafo dos"
+        description1: "En Variedades Nubia, ofrecemos una mezcla perfecta de diversión y sabor. Desde nuestra juguetería, llena de productos que alegrarán a los más pequeños, hasta nuestras deliciosas obleas y helados, tenemos algo para todos.",
+        description2: "Nos especializamos en proporcionar sonrisas y sabores únicos para todas las edades. Nuestro objetivo es que cada visita sea una experiencia memorable, ya sea que estés buscando el juguete perfecto o simplemente quieras disfrutar de un dulce capricho."
     },
     contact: {
         whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`,
-        email: email
+        email: email,
     },
     footer: {
-        slogan:{
-            p1: "slogan del emprendimiento parrafo uno",
-            p2: "slogan del emprendimiento parrafo dos"
+        slogan: {
+            p1: "En Variedades Nubia, cada día es una celebración de sonrisas y sabores.",
+            p2: ""
         },
-        address: "Dirección del emprendimiento",
+        address: "", // Información no proporcionada
         phone: numeroWhatsApp,
         email: email,
-        facebook: "https://www.facebook.com/",
-        instagram: "https://www.instagram.com/",
-        whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`
+        facebook: "https://www.facebook.com/profile.php?id=61565632154784&mibextid=ZbWKwL",
+        instagram: "https://www.instagram.com/variedades_.sofia?igsh=Z3NhZ20yOWVqem84",
+        whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`,
     },
-
 };
+
 export default textos;
